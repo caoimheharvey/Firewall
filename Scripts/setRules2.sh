8e0257d001d0525666ebffbd58e99ca331c99534
Pamela Sabio <pamelalouisse@yahoo.com>
2017-05-16 13:17:16 +0800

    edited setrules2

--- Scripts/setRules2.sh
+++ Scripts/setRules2.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 #Script to set up ip tables rules.
-#Pamela Sabio 16/05/17 
+#Eryk Szlachetka & Caoimhe Harvey 18/04/17 
 
 echo Setting SSH INPUT..
 #Allow established input SSH connection
-iptables -A INPUT -i eth0 -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -o eth0 -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+
+echo Setting SSH OUTPUT..
+#Allow NEW,ESTABLISHED output SSH connection
+iptables -A OUTPUT -p tcp -m tcp  --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
 echo Setting HTTP/S INPUT..
 #Allow NEW,ESTABLISHED,RELATED http and https output connections
-iptables -A INPUT -i eth0 -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -o eth0 -p tcp --sport 80 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp -m multiport --dports 80,433 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 
-echo Setting SSH OUTPUT..
-iptables -A OUTPUT -o eth0 -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -i eth0 -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
+echo Setting HTTP/S OUTPUT..
+#Allow ESTABLISHED,RELATED http and https input connections
+iptables -A OUTPUT -p tcp -m multiport --sports 80,433 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
-echo Setting HTTP/S OUTPUT
-iptables -A OUTPUT -o eth0 -p tcp --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -i eth0 -p tcp --sport 443 -m state --state ESTABLISHED -j ACCEPT
+echo Setting UDP/TCP OUTPUT..
+#Allow NEW udp/tcp output connections
+iptables -A OUTPUT -m state --state NEW -p udp --sport 53 -j ACCEPT
+iptables -A OUTPUT -m state --state NEW -p tcp --sport 53 -j ACCEPT
+
+echo Setting UDP/TCP INPUT..
+#Allow ESTABLISHED udp/tcp input connections
+iptables -A INPUT -m state --state NEW,ESTABLISHED -p udp --dport 53 -j ACCEPT
+iptables -A INPUT -m state --state NEW,ESTABLISHED -p tcp --dport 53 -j ACCEPT
+
+echo Setting SMTP INPUT..
+#Allow SMTP connections INPUT
+iptables -A INPUT -p tcp -m tcp --dport 25 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+
+echo Setting SMTP OUTPUT..
+#Allow SMTP connection OUTPUT
+iptables -A OUTPUT -p tcp -m tcp --sport 25 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
 echo Setting Forwarding..
 #setting IP Forwarding for internal network
